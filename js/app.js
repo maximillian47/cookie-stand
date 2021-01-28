7ca255510b1600b23bc06b1d4386e7140e13222e
maximillian47 <xiao.ma_1@outlook.com>
2021-01-27 23:29:17 -0800

    objects update 1

--- js/app.js
+++ js/app.js
@@ -6,13 +6,40 @@ let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 let seattle = {
   name: 'Seattle',
-  min: 23, 
-  max: 65, 
-  avg: 6.3, 
+  minimumCustomerEachHour: 23, 
+  maximumCustomerEachHour: 65, 
+  averageCookiesSoldPerCustomer: 6.3, 
+  // will hold the calculated number of cookies sold each hour
+  cookiesSoldPerHourArray:[],
+  // will hold the calculated number of cookies sold in the store all day long
+  dailyStoreTotal: 0,
+
   // a method to calculate random number of customers per hour
+  // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomerEachHour: function(){
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour) + this.minimumCustomerEachHour);
+  },
+
   // a method to calculate and populate number of cookies sold per hour array
+  calcCookiesSoldEachHour: function(){
+    let randomCustomerForOneHour = this.randomCustomerEachHour();
+    console.log(randomCustomerForOneHour);
+    console.log('calcCookiesSoldEachHour');
+  },
+
   // a method to render list items
-}
+  render: function(){
+    this.calcCookiesSoldEachHour();
+    console.log('render method');
+  },
+};
+
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+
 
 let tokyo = {
   name: 'Tokyo',
