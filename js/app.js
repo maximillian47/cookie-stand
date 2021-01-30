e88fa7b67c4fdb68dd94f95e307073aa95faea6d
maximillian47 <xiao.ma_1@outlook.com>
2021-01-30 00:51:54 -0800

    lab06 final revision for full credit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log('hello world');
-
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm' ];
 
 let seattle = {
@@ -17,54 +15,167 @@ let seattle = {
   // a method to calculate random number of customers per hour
   // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   randomCustomerEachHour: function(){
-    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour) + this.minimumCustomerEachHour);
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
   },
-
+    
   // a method to calculate and populate number of cookies sold per hour array
   calcCookiesSoldEachHour: function(){
-    let randomCustomerForOneHour = this.randomCustomerEachHour();
-    console.log(randomCustomerForOneHour);
-    console.log('calcCookiesSoldEachHour');
+    console.log('calcCookiesSoldEachHour starts here');
+    
+    for (var i=0; i<14; i++){
+      let randomCustomerForOneHour = this.randomCustomerEachHour();
+      this.cookiesSoldPerHourArray[i] = Math.ceil(randomCustomerForOneHour*this.averageCookiesSoldPerCustomer); 
+    }
+    console.log(this.cookiesSoldPerHourArray);
+    return this.cookiesSoldPerHourArray; 
   },
 
   // a method to render list items
   render: function(){
     this.calcCookiesSoldEachHour();
-    console.log('render method');
+    console.log('render method runs here');
   },
 };
+
+let tokyo = {
+  name: 'Tokyo',
+  minimumCustomerEachHour: 3, 
+  maximumCustomerEachHour: 24, 
+  averageCookiesSoldPerCustomer: 1.2, 
+  cookiesSoldPerHourArray:[],
+  // will hold the calculated number of cookies sold in the store all day long
+  dailyStoreTotal: 0,
+
+  // a method to calculate random number of customers per hour
+  // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomerEachHour: function(){
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
+  },
+    
+  // a method to calculate and populate number of cookies sold per hour array
+  calcCookiesSoldEachHour: function(){
+    console.log('calcCookiesSoldEachHour starts here');
+    
+    for (var i=0; i<14; i++){
+      let randomCustomerForOneHour = this.randomCustomerEachHour();
+      this.cookiesSoldPerHourArray[i] = Math.ceil(randomCustomerForOneHour*this.averageCookiesSoldPerCustomer); 
+    }
+    console.log(this.cookiesSoldPerHourArray);
+    return this.cookiesSoldPerHourArray; 
+  },
+
+  // a method to render list items
+  render: function(){
+    this.calcCookiesSoldEachHour();
+    console.log('render method runs here');
+  },
+}
+
+let dubai = {
+  name: 'Dubai',
+  minimumCustomerEachHour: 11, 
+  maximumCustomerEachHour: 38, 
+  averageCookiesSoldPerCustomer: 3.7, 
+  cookiesSoldPerHourArray:[],
+  // will hold the calculated number of cookies sold in the store all day long
+  dailyStoreTotal: 0,
+
+  // a method to calculate random number of customers per hour
+  // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomerEachHour: function(){
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
+  },
+    
+  // a method to calculate and populate number of cookies sold per hour array
+  calcCookiesSoldEachHour: function(){
+    console.log('calcCookiesSoldEachHour starts here');
+    
+    for (var i=0; i<14; i++){
+      let randomCustomerForOneHour = this.randomCustomerEachHour();
+      this.cookiesSoldPerHourArray[i] = Math.ceil(randomCustomerForOneHour*this.averageCookiesSoldPerCustomer); 
+    }
+    console.log(this.cookiesSoldPerHourArray);
+    return this.cookiesSoldPerHourArray; 
+  },
+
+  // a method to render list items
+  render: function(){
+    this.calcCookiesSoldEachHour();
+    console.log('render method runs here');
+  },
+}
+
+let paris = {
+  name: 'Paris',
+  minimumCustomerEachHour: 20, 
+  maximumCustomerEachHour: 38, 
+  averageCookiesSoldPerCustomer: 2.3, 
+  cookiesSoldPerHourArray:[],
+  // will hold the calculated number of cookies sold in the store all day long
+  dailyStoreTotal: 0,
+
+  // a method to calculate random number of customers per hour
+  // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomerEachHour: function(){
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
+  },
+    
+  // a method to calculate and populate number of cookies sold per hour array
+  calcCookiesSoldEachHour: function(){
+    console.log('calcCookiesSoldEachHour starts here');
+    
+    for (var i=0; i<14; i++){
+      let randomCustomerForOneHour = this.randomCustomerEachHour();
+      this.cookiesSoldPerHourArray[i] = Math.ceil(randomCustomerForOneHour*this.averageCookiesSoldPerCustomer); 
+    }
+    console.log(this.cookiesSoldPerHourArray);
+    return this.cookiesSoldPerHourArray; 
+  },
+
+  // a method to render list items
+  render: function(){
+    this.calcCookiesSoldEachHour();
+    console.log('render method runs here');
+  },
+  
+}
+
+let lima = {
+  name: 'Lima',
+  minimumCustomerEachHour: 2, 
+  maximumCustomerEachHour: 16, 
+  averageCookiesSoldPerCustomer: 4.6, 
+  cookiesSoldPerHourArray:[],
+  // will hold the calculated number of cookies sold in the store all day long
+  dailyStoreTotal: 0,
+
+  // a method to calculate random number of customers per hour
+  // doc: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomCustomerEachHour: function(){
+    return Math.floor(Math.random() * (this.maximumCustomerEachHour - this.minimumCustomerEachHour + 1) + this.minimumCustomerEachHour);
+  },
+    
+  // a method to calculate and populate number of cookies sold per hour array
+  calcCookiesSoldEachHour: function(){
+    console.log('calcCookiesSoldEachHour starts here');
+    
+    for (var i=0; i<14; i++){
+      let randomCustomerForOneHour = this.randomCustomerEachHour();
+      this.cookiesSoldPerHourArray[i] = Math.ceil(randomCustomerForOneHour*this.averageCookiesSoldPerCustomer); 
+    }
+    console.log(this.cookiesSoldPerHourArray);
+    return this.cookiesSoldPerHourArray; 
+  },
+
+  // a method to render list items
+  render: function(){
+    this.calcCookiesSoldEachHour();
+    console.log('render method runs here');
+  },
+}
 
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
-
-
-let tokyo = {
-  name: 'Tokyo',
-  min: 3,
-  max: 24,
-  avg: 1.2,
-}
-
-let dubai = {
-  name: 'Dubai',
-  min: 11,
-  max: 38,
-  avg: 3.7,
-}
-
-let paris = {
-  name: 'Paris',
-  min: 20,
-  max: 38,
-  avg: 2.3,
-}
-
-let lima = {
-  name: 'Lima',
-  min: 2,
-  max: 16,
-  avg: 4.6,
-}
